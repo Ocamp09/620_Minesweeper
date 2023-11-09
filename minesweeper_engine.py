377404944a2f28feab98daa6754acee8eb496ad4
campbeo2 <campbeo2@miamioh.edu>
2023-11-09 09:48:17 -0500

    recursive reveal, first move, refactor, and predicates for ASP

--- minesweeper_engine.py
+++ minesweeper_engine.py
@@ -33,15 +33,23 @@ class MinesweeperGame:
             for j in range(height):
                 if self.mine_board[j][i] != "mine":
                     num_neighboring_mines = 0
-
+                    # view all neighboring cells and count the mines
                     for x in range(-1, 2):
                         for y in range(-1, 2):
-                            if 0 <= i + x < width and 0 <= j + y < height and self.mine_board[j + y][i + x] == "mine":
-                                num_neighboring_mines += 1
+                            # x and y coordinates of neighboring cells
+                            x_cord = i + x
+                            y_cord = j + y
 
+                            # if the neighboring coordinates are within the game board
+                            if 0 <= x_cord < self.width and 0 <= y_cord < self.height:
+                                # if the neighboring cell is a mine add one to the count
+                                if self.mine_board[y_cord][x_cord] == "mine":
+                                    num_neighboring_mines += 1
+
+                    # set the number of neighboring mines
                     self.mine_board[j][i] = num_neighboring_mines
 
-    # Function used to display the current status of the game board in the terinal
+    # Function used to display the current status of the game board in the terminal
     def display_board(self):
         # Print each row of the board.
         for row in self.game_board:
@@ -58,20 +66,41 @@ class MinesweeperGame:
         print("-" * self.width * 2)
 
     # method to reveal a cell in the game board, returning true kills the game while loop, false continues
-    def reveal_cell(self, x, y):
+    def reveal_cell(self, x, y, recursive=False):
         # if the cell revealed is a mine, then the game is lost
         if self.mine_board[y][x] == "mine":
             print("Game lost")
             # return True
         # if the current cell is already empty print a message
-        elif self.game_board[y][x] != " ":
+        elif self.game_board[y][x] != " " and not recursive:
             print("Cannot play in an already revealed cell")
         # if the cell is not revealed and not a mine then update the cell from the mine board
         else:
+            # reveal the selected cell
             self.game_board[y][x] = self.mine_board[y][x]
-            print("New board status")
 
-        # if the game si won then print a message and stop the while loop
+            # if the danger level of revealed cell is zero make recursive calls
+            if self.game_board[y][x] == 0:
+                # loop through neighboring cells
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        # x and y coordinates of neighboring cells
+                        x_cord = i + x
+                        y_cord = j + y
+
+                        # if the neighboring coordinates are within the game board
+                        if 0 <= x_cord < self.width and 0 <= y_cord < self.height:
+                            # if the cell is already revealed skip, else if the cell has no neighboring mines
+                            # make the recursive call to reveal neighboring zero cells, else reveal the first layer
+                            # touching the zero cells
+                            if self.game_board[y_cord][x_cord] != " ":
+                                continue
+                            elif self.mine_board[y_cord][x_cord] == 0:
+                                self.reveal_cell(x_cord, y_cord, True)
+                            else:
+                                self.game_board[y_cord][x_cord] = self.mine_board[y_cord][x_cord]
+
+        # if the game is won then print a message and stop the while loop with a true return
         if self.is_game_won():
             print("Game won")
             return True
@@ -80,55 +109,77 @@ class MinesweeperGame:
 
     # function evaluates if all the cells in game board are either not empty, or not mines, signifying the game is over
     def is_game_won(self):
-        for i in range(self.width):
-            for j in range(self.height):
+        # loop through the game/mine board
+        for row in range(self.width):
+            for col in range(self.height):
                 # If there is an empty space that is not a mine, the game is not won
-                if not self.mine_board[j][i] == "mine" and self.game_board[j][i] == " ":
+                if not self.mine_board[col][row] == "mine" and self.game_board[col][row] == " ":
                     return False
 
         return True
 
+    # write the danger levels to the asp file
+    def write_to_file(self):
+        game_asp_file = "game_board_data.lp"
 
-# run the clingo file to get the next best move
+        # write the game_board board to a file to be opened by the clingo
+        with open('game_board_data.lp', 'w') as file:
+            file.write("board_size({0}, {1}).\n".format(self.width, self.height))
+
+            # write the revealed cells danger numbers to the asp file
+            for col in range(self.height):
+                for row in range(self.width):
+                    if self.game_board[col][row] != " ":
+                        file.write("danger_level({0},{1},{2}).\n".format(row, col, self.mine_board[col][row]))
+
+        # Create a subprocess object to run the ASP code
+        p = subprocess.Popen(["clingo", player_asp_file, game_asp_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        # Receive the output from the ASP code and wait for the subprocess to finish
+        asp_response = p.stdout.read().decode("utf-8")
+        p.wait()
+
+        return self.parse_asp(asp_response)
+
+    # method to take the raw asp response and turn it into the next coordinates to play
+    def parse_asp(self, response):
+        print(response)
+        return response
+
+    # function to reveal a random cell/cells to give the ASP code a starting point
+    def first_move(self):
+        # get random x and y coordinates
+        rand_x = random.randint(0, game.width - 1)
+        rand_y = random.randint(0, game.height - 1)
+
+        # if the coordinates are mines then try again, else reveal the cell
+        if self.game_board[rand_y][rand_x] == "mine":
+            self.first_move()
+        else:
+            self.reveal_cell(rand_x, rand_y)
 
 
 # Start the game
-game = MinesweeperGame(10, 10, 10)
+game = MinesweeperGame(5, 5, 3)
 game_over = False
 
-game_board_str = ""
-game_board_str = game_board_str.join(map(str, game.game_board))
-
-game_asp_file = "game_board_data.lp"
 player_asp_file = "minesweeper_player.lp"
 
-# write the game board to a file to be opened by the clingo
-with open('game_board_data.lp', 'w') as file:
-    file.write("board_size({0}, {1}).".format(game.width, game.height))
-    # file.write(game_board_str)
+game.first_move()
+print("Starting board: ")
+game.display_board()
+next_cords = game.write_to_file()
 
-# Create a subprocess object to run the ASP code
-p = subprocess.Popen(["clingo", player_asp_file, game_asp_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-# Receive the output from the ASP code and wait for the subprocess to finish
-asp_response = p.stdout.read().decode("utf-8")
-p.wait()
-
-# get move from ASP
-print(asp_response)
-
-# some starting logic to get revealed cells??
-
-# loop until game is over
-# while not game_over:
-#     # get game board for ASP
-#
+# # temp variable to stop infinite loops, remove later
+# temp = 0
+# # loop until game is over
+# while temp <= 3 and not game_over:
 #     # send game board to ASP and get response in return
+#     next_cords = game.write_to_file()
 #
 #     # add ASP response to reveal_cell
-#     game_over = game.reveal_cell(random.randint(0, game.width - 1), random.randint(0, game.height - 1))
+#     # game_over = game.reveal_cell(random.randint(0, game.width - 1), random.randint(0, game.height - 1))
 #
 #     # display new board to see ASP order of actions
-#     game.display_board()
-
-
+#     # game.display_board()
+#     temp += 1
